fc9a69ac8ecc7105b745e16c73be07da1868ae51
kawsar-uddin <kawsaruddin450@gmail.com>
2024-08-02 11:51:53 +0600

    setup news API

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.use(cors());
 
@@ -11,10 +12,28 @@ app.get('/', (req, res) => {
     res.send('Dragon is running!');
 });
 
-app.get('/categories', (req, res)=> {
+app.get('/categories', (req, res) => {
     res.send(categories);
 })
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '0') {
+        res.send(news);
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews);
+    }
+})
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+})
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`Dragon API is running on Port: ${port}`);
 });
